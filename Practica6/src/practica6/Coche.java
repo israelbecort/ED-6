cbc614f839ed463e5dd846961f6c0826c182cc09
Israel <Israel@DESKTOP-AM2T499>
2019-05-31 18:34:17 +0200

    #3 y #4

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -2,39 +2,57 @@ package practica6;
 
 public class Coche {
 	String matricula;
-	String atrib;
+	String motor;
 	String modelo;
 	String fabricante;
 
 	public Coche() {
 		matricula = "";
-		atrib = "";
+		motor = "";
 		modelo = "";
 		fabricante = "";
 	}
 
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
-		atrib = c;
+		motor = c;
 		modelo = mo;
 		fabricante = f;
 	}
 
-	public String metodo1() {
+	public String caracteristicasCoche() {
 		String resultado = "";
-		if (atrib == "Gasolina") {
+		if (motor == "Gasolina") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
-		} else if (atrib == "Diesel") {
+			resultado += cocheGasolina(modelo, fabricante);
+		} else if (motor == "Diesel") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
-		} else if (atrib == "Híbrido") {
+			resultado += cocheDiesel(modelo, fabricante);
+		} else if (motor == "Híbrido") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
+			resultado += cocheHibrido(modelo, fabricante);
 		} else {
 			resultado += "El coche con matricula " + matricula + " ";
 			resultado += "no dispone de información";
 		}
+		return resultado;
+	}
+
+	private String cocheHibrido(String modelo, String fabricante)
+	{
+		String resultado="es un"+fabricante+modelo+"y gasta 1,337 euros por litro.";
+		return resultado;
+	}
+
+	private String cocheDiesel(String modelo, String fabricante)
+	{
+		String resultado="es un"+fabricante+modelo+"y gasta 1,052 euros por litro.";
+		return resultado;
+	}
+
+	private String cocheGasolina(String modelo, String fabricante)
+	{
+		String resultado="es un"+fabricante+modelo+"y no necesita combustible.";
 		return resultado;
 	}
 }
